9bea621ba2f28a611e80c036470b770c271343c1
Harshavardhan Reddy <57401710+theh4cker1769@users.noreply.github.com>
2021-09-20 01:28:19 +0530

    Comment Input

--- index.php
+++ index.php
@@ -1,10 +1,32 @@
 <?php
 session_start();
-
 include("connection.php");
 include("functions.php");
 
 $user_data = check_login($conn);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_SESSION['user_id'])) {
+
+        $userid = $_SESSION['user_id'];
+        $message = $_POST['commentmsg'];
+        $getname = "select name from users where user_id = '$userid'";
+        $result = mysqli_query($conn, $getname);
+
+        if ($result) {
+            $row = mysqli_fetch_array($result);
+            $name = $row['name'];
+        }
+
+        $query = "insert into comments (name, message) values('$name', '$message')";
+        mysqli_query($conn, $query);
+
+    }
+    else {
+        echo '<script>alert("Please Login")</script>';
+    }
+}
+
 
 ?>
 
@@ -24,6 +46,12 @@ $user_data = check_login($conn);
     <a href="logout.php">Logout</a>
     <h1>Hello World From Israel and Harshavardhan Reddy</h1>
     <h2>Hello <?php echo $user_data['username'];  ?> </h2>
+
+    <img src="images/tree-736885__480.jpg" alt="Image">
+    <form action="" method="post">
+        <textarea name="commentmsg" cols="30" rows="10" placeholder="Comment"></textarea>
+        <input type="submit" value="Submit">
+    </form>
 </body>
 
 </html>
--- signup.php
+++ signup.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
+
     include("connection.php");
     include("functions.php");
-
 
     if($_SERVER['REQUEST_METHOD'] == 'POST')
     {
